6580f4e40befc30704c0fcac3a9cb291bace897a
igasz <igaszaf@gmail.com>
2026-07-23 11:31:50 +0200

    adding images

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,18 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
+# model for images
+class ListingImage(Base):
+    __tablename__ = 'listing_images'
+    image_url = Column(String, primary_key=True)
+
+    listing_id = Column(String, ForeignKey('listings.listing_id'))
+    is_main_photo = Column(Integer)
+
+    listing = relationship("Listing", back_populates="images")
+
+# listing model
 class Listing(Base):
     __tablename__ = 'listings'
 
@@ -17,3 +29,5 @@ class Listing(Base):
     home_type = Column(String)
     description = Column(String)
     current_status = Column(String)
+
+    images = relationship("ListingImage", back_populates="listing")
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,14 @@
 from pydantic import BaseModel
 
+# schema for images
+class ImageResponse(BaseModel):
+    image_url: str
+    is_main_photo: int
+
+    class Config:
+        from_attributes = True
+
+# listing schema
 class ListingResponse(BaseModel):
     listing_id: str
     price: float
@@ -7,6 +16,8 @@ class ListingResponse(BaseModel):
     bedrooms: float
     bathrooms: float
     living_area: float
+
+    images: list[ImageResponse] = []
 
     # from sqlalchemy object
     class Config:
