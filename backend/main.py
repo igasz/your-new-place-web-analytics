48b5add524c4a7c85f6063347b4a99af3e52e5d1
igasz <igaszaf@gmail.com>
2026-07-23 11:17:59 +0200

    updating main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
-from sqlalchemy import text
 from sqlalchemy.orm import Session
+
 from database import get_db
+import models
+import schemas
 
 # Initialize the FastAPI app
 app = FastAPI(title="Your New Place API")
@@ -14,25 +16,6 @@ def read_root():
 # fetching data from Oracle
 @app.get("/listings")
 def get_listings(db: Session = Depends(get_db)):
-    # raw SQL query to get 5 properties
-    query = text("""
-        SELECT listing_id, price, street_address, bedrooms, bathrooms, living_area 
-        FROM listings 
-        FETCH FIRST 5 ROWS ONLY
-    """)
+    listings = db.query(models.Listing).limit(5).all()
     
-    result = db.execute(query)
-    
-    # Format the SQL results into a clean dictionary list
-    listings_data = []
-    for row in result:
-        listings_data.append({
-            "id": row[0],
-            "price": row[1],
-            "address": row[2],
-            "beds": row[3],
-            "baths": row[4],
-            "sqft": row[5]
-        })
-        
-    return {"listings": listings_data}
+    return listings
