ba28b4cbe080e080911219f3f56982ad0cf6ebc4
igasz <igaszaf@gmail.com>
2026-07-23 11:10:50 +0200

    access to database and fetching first data from oracle

---
+++ backend/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# the same connection
+DB_URL = "oracle+oracledb://yourplace:Password123@localhost:1521/?service_name=FREEPDB1"
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# give our API access to the database
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from database import get_db
 
 # Initialize the FastAPI app
 app = FastAPI(title="Your New Place API")
 
-# Create our first "endpoint" (a URL route)
+# first "endpoint" (a URL route)
 @app.get("/")
 def read_root():
     return {"message": "Your New Place API!"}
 
-@app.get("/status")
-def api_status():
-    return {"status": "Online", "database_connected": False}
+# fetching data from Oracle
+@app.get("/listings")
+def get_listings(db: Session = Depends(get_db)):
+    # raw SQL query to get 5 properties
+    query = text("""
+        SELECT listing_id, price, street_address, bedrooms, bathrooms, living_area 
+        FROM listings 
+        FETCH FIRST 5 ROWS ONLY
+    """)
+    
+    result = db.execute(query)
+    
+    # Format the SQL results into a clean dictionary list
+    listings_data = []
+    for row in result:
+        listings_data.append({
+            "id": row[0],
+            "price": row[1],
+            "address": row[2],
+            "beds": row[3],
+            "baths": row[4],
+            "sqft": row[5]
+        })
+        
+    return {"listings": listings_data}
